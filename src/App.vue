6bee818b8bf6a0f0c10672d714711e95d254ead6
leaPortier74 <lea.portier@hotmail.fr>
2023-06-06 16:39:58 +0200

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <h2 class="text-2xl font-bold text-blue-800">
+        how to start your project with tailwind css & Vue 3
+      </h2>  
+    </div>
+  </div>
 </template>
 
 <script>
+import '@/assets/tailwind.css';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
