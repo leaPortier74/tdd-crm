edb699fe35605864729b494b4f887426f6fd57b8
leaPortier74 <lea.portier@hotmail.fr>
2023-06-23 10:43:23 +0200

    re design

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="container max-w-7xl mx-auto px-4">
+    <div class="container max-w-7xl mx-auto px-4" style="margin-top: 2rem;">
         <div v-if="users" class="flex flex-wrap w-full ">
             <div v-for="user in users" v-bind:key="`user-${user.id}`"
                 class="fw-full md:w-6/12 lg:w-3/12 lg:mb-3 mb-12 px-4">
-                <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                <div class="bg-white border border-gray-200 rounded-lg shadow "
                     style="padding: 2rem; margin: 0.2rem;">
                     <img class="w-24 h-24 mb-3 rounded-full shadow-lg" style="display: initial;"
                         v-bind:alt="`${user.first_name} ${user.last_name}`" v-bind:src="`${user.avatar}`" />
-                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ user.first_name }} {{
+                    <h5 class="mb-1 text-xl font-medium text-gray-900">{{ user.first_name }} {{
                         user.last_name }}</h5>
                     <div class="mt-4 space-x-3 md:mt-6">
                         <router-link v-bind:to="`/user/${user.id}`" custom v-slot="{ navigate }">
-                            <button @click="navigate" role="link"
-                                class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Plus
+                            <button @click="navigate" role="link" style="background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);"
+                                class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none ">Plus
                                 d'information</button>
                         </router-link>
                     </div>
@@ -22,7 +22,7 @@
     </div>
     <div>
         <router-link to="/" custom v-slot="{ navigate }">
-            <button id="logout" style="margin: 2rem 0; width: 50%;" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="navigate" role="link">Logout</button>
+            <button id="logout" style="margin: 2rem 0; width: 50%; background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);" class="items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg  focus:ring-4 focus:outline-none " @click="navigate" role="link">Logout</button>
         </router-link>
     </div>
 </template>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,10 @@
 <template>
   <router-link to="/dashboard" custom v-slot="{navigate} ">
-    <button id="dashboard"  style="margin: 2rem 0; width: 15%;" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  @click="navigate" role="link">Return</button>
+    <button id="dashboard"  style="margin: 2rem 0; width: 10%; background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);" class="text-white bg-gradient-to-r focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  @click="navigate" role="link">Return</button>
   </router-link>
   <div class="user">
     
-<h1 class="mb-4 mt-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Bienvenue sur votre profile</span></h1>
+<h1 class="mb-4 mt-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-violet-800 from-red-600">Bienvenue sur votre profile</span></h1>
 
     <div class="" v-if="user">
       <img v-bind:alt="`${user.first_name} ${user.last_name}`" v-bind:src="`${user.avatar}`" class="rounded-xl shadow-lg mx-auto border-none" style="width: 15%;">
@@ -27,7 +27,7 @@
   </div>
   <div>
     <router-link to="/" custom v-slot="{navigate} ">
-        <button id="logout" style="margin: 2rem 0; width: 25%;" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  @click="navigate" role="link">Logout</button>
+        <button id="logout" style="margin: 2rem 0; width: 25%; background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);" class="text-white to-bl focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"  @click="navigate" role="link">Logout</button>
     </router-link>
   </div>
 </template>
