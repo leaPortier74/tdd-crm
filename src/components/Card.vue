7d06fcdc56c13b077b3d0a928002fd65a789bbec
leaPortier74 <lea.portier@hotmail.fr>
2023-06-23 09:21:48 +0200

    style card user

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,39 +1,30 @@
 <template>
-    <div style='background-color:rgba(0, 0, 0, 0)'>
-        <div class="container max-w-7xl mx-auto px-4" style="cursor: auto;">
-            <div class="flex flex-wrap" v-if="users">
-                <div v-for="user in users" v-bind:key="`user-${user.id}`" class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-                    <router-link v-bind:to="`/user/${user.id}`" custom v-slot="{navigate} ">
-                        <div class="px-6" @click="navigate" role="link">
-                            <img v-bind:alt="`${user.first_name} ${user.last_name}`" v-bind:src="`${user.avatar}`" class="rounded-xl shadow-lg max-w-full h-auto align-middle border-none undefined">
-                            <div class="pt-6 text-center">
-                                <h1 class="text-gray-900 text-xl font-serif font-bold leading-normal mt-0 mb-2">{{ user.first_name }} {{ user.last_name }}</h1>
-                                <div class="flex items-center justify-center">
-                                    <button
-                                        class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-full w-10 h-10 p-0 grid place-items-center text-xs leading-normal bg-transparent text-light-blue-500 hover:bg-light-blue-50 hover:text-light-blue-700 hover:bg-light-blue-50 active:bg-light-blue-100 undefined">
-                                        <i class="fab fa-twitter undefined text-lg"></i>
-                                    </button>
-                                    <button
-                                        class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-full w-10 h-10 p-0 grid place-items-center text-xs leading-normal bg-transparent text-blue-500 hover:bg-blue-50 hover:text-blue-700 hover:bg-blue-50 active:bg-blue-100 undefined">
-                                        <i class="fab fa-facebook-f undefined text-lg"></i>
-                                    </button>
-                                    <button
-                                        class="false flex items-center justify-center gap-1 font-bold outline-none uppercase tracking-wider focus:outline-none focus:shadow-none transition-all duration-300 rounded-full w-10 h-10 p-0 grid place-items-center text-xs leading-normal bg-transparent text-pink-500 hover:bg-pink-50 hover:text-pink-700 hover:bg-pink-50 active:bg-pink-100 undefined">
-                                        <i class="fab fa-dribbble undefined text-lg"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </router-link>
+    <div class="container max-w-7xl mx-auto px-4">
+        <div v-if="users" class="flex flex-wrap w-full ">
+            <div v-for="user in users" v-bind:key="`user-${user.id}`"
+                class="fw-full md:w-6/12 lg:w-3/12 lg:mb-3 mb-12 px-4">
+                <div class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                    style="padding: 2rem; margin: 0.2rem;">
+                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" style="display: initial;"
+                        v-bind:alt="`${user.first_name} ${user.last_name}`" v-bind:src="`${user.avatar}`" />
+                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ user.first_name }} {{
+                        user.last_name }}</h5>
+                    <div class="mt-4 space-x-3 md:mt-6">
+                        <router-link v-bind:to="`/user/${user.id}`" custom v-slot="{ navigate }">
+                            <button @click="navigate" role="link"
+                                class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Plus
+                                d'information</button>
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <div>
-        <router-link to="/" custom v-slot="{navigate} ">
-            <button id="logout" @click="navigate" role="link">Logout</button>
+        <router-link to="/" custom v-slot="{ navigate }">
+            <button id="logout" style="margin: 2rem 0; width: 50%;" class="items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" @click="navigate" role="link">Logout</button>
         </router-link>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -42,21 +33,21 @@ import '@/assets/tailwind.css';
 
 
 export default {
-  name: 'CardUsers',
-  mounted(){
-    fetch('https://reqres.in/api/users')
-    .then(response => response.json())
-    .then(data => {
-    const users = data.data;
-    this.users = users
-    })
-    .catch(error => console.error(error));
-  },
-  data(){
-    return{
-        users: this.users
+    name: 'CardUsers',
+    mounted() {
+        fetch('https://reqres.in/api/users')
+            .then(response => response.json())
+            .then(data => {
+                const users = data.data;
+                this.users = users
+            })
+            .catch(error => console.error(error));
+    },
+    data() {
+        return {
+            users: this.users
+        }
+
     }
-    
-  }
 }
 </script>
